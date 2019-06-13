bae3d2f14615edd88c1f1cd27d6c85f5b3c5cb29
Sean Gargiulo <spg363@stern.nyu.edu>
2019-06-12 20:16:23 -0400

    failed stock prices attempt

--- omniparser/stock_prices_parser.py
+++ omniparser/stock_prices_parser.py
@@ -1,2 +1,34 @@
 
-print("PARSING SOME STOCK PRICES HERE...")
+# omniparser/gradebook_parser.py
+
+import os
+import statistics
+import json
+import pandas
+
+
+def calculate_average_stock_from_json(x):
+    
+
+    with open(x, "r") as f:
+        #print(type(f))
+        file_contents = f.read()
+        #print(type(file_contents))
+
+    stock_list = json.loads(file_contents)
+    #print(type(gradebook))
+    #print(gradebook)
+    stocks = stock_list ["stock_prices"]
+    stock_prices = [s["4. close"] for s in stocks]
+    avg_stock_price = statistics.mean(stock_prices)
+    
+    return avg_stock_price
+
+if __name__ == "__main__":   
+
+    print("PARSING SOME JSON STOCK FILES HERE...")
+    app_stock_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "stock_prices_aapl.json")
+    print(app_stock_filepath)
+    print(os.path.isfile(app_stock_filepath))
+    avg = calculate_average_stock_from_json(app_stock_filepath)
+    print(avg)
